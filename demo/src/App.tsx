fccee837444723d2d8ecdc5a041a105ff35b700a
pl <403802162@qq.com>
2024-08-12 14:42:06 +0800

    更新

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -6,6 +6,8 @@ import { useRef, useState } from "react";
 import ChatEditor, { Version } from "../../src";
 import "./App.css";
 
+console.log(Version);
+
 function App() {
   const [v, setVal] = useState("");
   const editorRef = useRef();
@@ -18,11 +20,12 @@ function App() {
     editorRef.current?.focus();
   };
 
-  console.log(v);
-
   return (
     <>
       <p style={{ fontSize: "30px", textAlign: "center", fontWeight: "bold" }}>fish-chat-editor</p>
+      <a href="https://github.com/u-fish-bubble/fish-chat-editor/blob/main/README.md" target="_blank">
+        fish-chat-editor文档
+      </a>
       <div style={{ marginTop: "180px" }}>
         <ChatEditor onEnterDown={onSend} onSend={onSend} ref={editorRef} onChange={(v) => setVal(v)} />
       </div>
