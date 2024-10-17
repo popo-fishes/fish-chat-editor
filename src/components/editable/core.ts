4d6f0f6e8612888c36c7b767d62572eb3086fa95
PL196\40380 <403802162@qq.com>
2024-10-17 19:32:02 +0800

    更新

---
+++ src/components/editable/core.ts
@@ -0,0 +1,283 @@
+import isObject from "lodash/isObject";
+
+import { regContentImg, labelRep } from "../../utils";
+
+import { dom, isNode, range as fishRange, editor, helper, util, base, transforms } from "../../core";
+
+import type { IEditorElement } from "../../types";
+
+const { isEditElement } = isNode;
+
+const { createLineElement, getElementAttributeKey, prefixNmae } = base;
+
+const { insertText, insertNode } = editor;
+
+const { fileToBase64, getRandomWord } = helper;
+
+/** @name 处理复制事件 */
+export const onCopy = (event: React.ClipboardEvent<HTMLDivElement>) => {
+  // 阻止默认事件，防止复制真实发生
+  event.preventDefault();
+
+  const selection = window.getSelection();
+
+  if (!selection) {
+    return;
+  }
+
+  if (selection.isCollapsed) {
+    return;
+  }
+
+  // 获取当前光标选中的内容，我们需要吧复制的内容转换一次
+  // cloneContents很关键，获取选中的文档碎片（此方法返回从Range的内容创建的DocumentFragment对象）
+  const contents = selection.getRangeAt(0)?.cloneContents();
+
+  // 将内容添加到＜div＞中，这样我们就可以获得它的内部HTML。
+  const odiv = contents.ownerDocument.createElement("div");
+  odiv.appendChild(contents);
+
+  odiv.setAttribute("hidden", "true");
+  contents.ownerDocument.body.appendChild(odiv);
+
+  const content = transforms.getPlainText(odiv);
+
+  // event.clipboardData.setData("text/html", odiv.innerHTML);
+  event.clipboardData?.setData("text/plain", content);
+  contents.ownerDocument.body.removeChild(odiv);
+};
+
+/** @name 处理剪切事件 */
+export const onCut = (event: React.ClipboardEvent<HTMLDivElement>) => {
+  event.preventDefault();
+  const selection = window.getSelection();
+  // 存在选区
+  if (selection && !selection.isCollapsed) {
+    /**
+     * 1；设置复制内容
+     */
+    // 获取当前光标选中的内容，我们需要吧复制的内容转换一次
+    // cloneContents很关键，获取选中的文档碎片（此方法返回从Range的内容创建的DocumentFragment对象）
+    const contents = selection.getRangeAt(0)?.cloneContents();
+
+    // 将内容添加到＜div＞中，这样我们就可以获得它的内部HTML。
+    const odiv = contents.ownerDocument.createElement("div");
+    odiv.appendChild(contents);
+
+    odiv.setAttribute("hidden", "true");
+    contents.ownerDocument.body.appendChild(odiv);
+
+    const content = transforms.getPlainText(odiv);
+
+    // event.clipboardData.setData("text/html", odiv.innerHTML);
+    event.clipboardData?.setData("text/plain", content);
+    contents.ownerDocument.body.removeChild(odiv);
+
+    /**
+     * 删除选区
+     *  */
+    // 后续可以拓展删除节点方法，先原生的
+    document.execCommand("delete", false, undefined);
+  }
+};
+
+/**
+ * @name 处理换行
+ */
+export const handleLineFeed = (editNode: IEditorElement, callBack?: (success: boolean) => void) => {
+  const range = fishRange.getRange();
+  // 必须存在光标
+  if (!range) return callBack(false);
+
+  // 获取当前光标的节点
+  const rangeStartContainer: any = range.startContainer;
+
+  // 行属性节点
+  const rowElementNode = util.getNodeOfEditorElementNode(rangeStartContainer);
+
+  if (!rowElementNode) {
+    // 非常重要的逻辑--修正光标位置
+    fishRange.amendRangePosition(editNode, (node) => {
+      if (node) {
+        // 在调用自己一次
+        handleLineFeed(editNode, callBack);
+      }
+    });
+    return callBack(false);
+  }
+
+  const [behindNodeList, nextNodeList] = dom.getRangeAroundNode(range);
+
+  console.time("editable插入换行耗时");
+
+  /**
+   * 创建换行节点
+   * @dec 把之前的节点放到需要换行的节点后面
+   */
+  // 创建换行节点
+  const lineDom = createLineElement(true);
+
+  if (!isEditElement(rowElementNode as HTMLElement)) {
+    console.warn("无编辑行节点，不可插入");
+    return callBack(false);
+  }
+
+  /**
+   * 把后面的节点放到换行节点的 文本节点中
+   */
+  const clNodes = dom.cloneNodes(nextNodeList);
+
+  // 存在换行元素时
+  if (clNodes.length) {
+    dom.toTargetAddNodes(lineDom, clNodes, false);
+
+    dom.removeNodes(nextNodeList);
+  } else {
+    const br = document.createElement("br");
+    dom.toTargetAddNodes(lineDom, [br]);
+  }
+
+  // 如果前面的节点不存在，后面的节点存在； 代表换行后，前面的节点是没有内容的
+  if (behindNodeList.length == 0 && nextNodeList.length) {
+    const br = document.createElement("br");
+    dom.toTargetAddNodes(rowElementNode, [br]);
+  }
+
+  dom.toTargetAfterInsertNodes(rowElementNode, [lineDom]);
+
+  // 第一个节点是一个文本节点
+  if (lineDom.firstChild) {
+    fishRange.setCursorPosition(lineDom.firstChild, "before");
+    lineDom?.scrollIntoView(true);
+
+    console.timeEnd("editable插入换行耗时");
+
+    // 执行回调
+    callBack?.(true);
+    return;
+  }
+
+  console.timeEnd("editable插入换行耗时");
+
+  callBack?.(false);
+};
+
+/**
+ * @name 处理粘贴事件的内容转换
+ * @param e 粘贴事件
+ * @param editNode 编辑器节点
+ * @param callBack 成功回调
+ */
+export const handlePasteTransforms = (e: ClipboardEventWithOriginalEvent, editNode: IEditorElement, callBack?: () => void) => {
+  // 获取粘贴的内容
+  const clp = e.clipboardData || (e.originalEvent && (e.originalEvent as any).clipboardData);
+  const isFile = clp?.types?.includes("Files");
+  const isHtml = clp?.types?.includes("text/html");
+  const isPlain = clp?.types?.includes("text/plain");
+
+  /**
+   * @name 如果是文件
+   */
+  if (isFile) {
+    const files = isObject(clp.files) ? Object.values(clp.files) : clp.files;
+    // console.log(files);
+    // 必须是图片
+    const vfiles = files?.filter((item) => item.type.includes("image/"));
+    // 截取前面5个文件
+    const sfiles = vfiles.slice(-5);
+
+    const promiseData = [];
+    // 处理获取到的文件
+    for (let i = 0; i < sfiles.length; i++) {
+      const file = sfiles[i];
+      promiseData.push(fileToBase64(file));
+    }
+    Promise.allSettled(promiseData)
+      .then((res) => {
+        const datas = [];
+        // 请求结束后再去清除掉
+        res.forEach((result) => {
+          if (result.status == "fulfilled" && result.value) {
+            datas.push(`data:image/jpeg;base64,${result.value}`);
+          }
+        });
+
+        const nodes: HTMLSpanElement[] = [];
+        datas.forEach((baseItem) => {
+          // 创建一个图片容器节点
+          const container = document.createElement("span");
+          container.id = `${prefixNmae}image-container-` + getRandomWord();
+          container.classList.add(`${prefixNmae}image-container`);
+
+          const node = new Image();
+          node.src = baseItem;
+          const key = getElementAttributeKey("imgNode");
+          node.setAttribute(key, "true");
+
+          container.appendChild(node);
+
+          const textNode = createChunkTextElement();
+
+          nodes.push(...[createChunkSapnElement(container), textNode]);
+        });
+
+        insertNode(nodes);
+      })
+      .catch(() => {});
+  }
+
+  /**
+   * @name 如果是文本
+   */
+  if ((isHtml || isPlain) && !isFile) {
+    // 如果是粘贴的是文本
+    const content = clp.getData("text/plain");
+    // x.getSelection
+    const selection = window.getSelection();
+    // 把文本标签转义：如<div>[爱心]</div> 把这个文本转义为"&lt;div&lt;",
+    const repContent = labelRep(content);
+
+    if (!repContent) {
+      isPasteLock = false;
+      return;
+    }
+
+    if (isPasteLock) return;
+
+    // 存在选区
+    if (selection && !selection.isCollapsed) {
+      // 后续可以拓展删除节点方法，先原生的
+      document.execCommand("delete", false, undefined);
+    }
+
+    // 在删除完成后执行其他操作
+    {
+      isPasteLock = true;
+
+      // 获取当前光标
+      const range = selection?.getRangeAt(0);
+
+      // 获取当前光标的开始容器节点
+      const topElementNode = findNodetWithElement(range.startContainer);
+
+      // 如果当前光标节点不是一个富文本元素节点，就默认指向它的第一个子节点
+      if (!topElementNode) {
+        // !!非常重要的逻辑
+        amendRangeLastNode(editNode, (node) => {
+          if (node) {
+            insertText(repContent, () => {
+              isPasteLock = false;
+              callBack?.();
+            });
+          }
+        });
+        return;
+      }
+
+      insertText(repContent, () => {
+        isPasteLock = false;
+        callBack?.();
+      });
+    }
+  }
+};
---
+++ src/components/editable/index.tsx
@@ -0,0 +1,127 @@
+/*
+ * @Date: 2024-09-30 15:40:27
+ * @LastEditors: Please set LastEditors
+ */
+import { forwardRef, useImperativeHandle } from "react";
+import type { IEmojiType, IEditableRef, IEditableProps } from "../../types";
+import { editor, transforms } from "../../core";
+import { labelRep } from "../../utils";
+
+import { onCopy, onCut } from "./core";
+
+import useEdit from "./useEdit";
+
+const Editable = forwardRef<IEditableRef, IEditableProps>((props, ref) => {
+  const { placeholder, ...restProps } = props;
+
+  const {
+    editRef,
+    showTipHolder,
+
+    setTipHolder,
+    setRangePosition,
+
+    clearEditor,
+
+    insertEmoji,
+
+    onEditorKeyUp,
+    onEditorChange,
+    onEditorBlur,
+    onEditorFocus,
+    onEditorClick,
+    onEditorKeydown,
+    onEditorPaste,
+    onCompositionStart,
+    onCompositionEnd,
+    onEditorMouseDown
+  } = useEdit(props);
+
+  useImperativeHandle(
+    ref,
+    () =>
+      ({
+        insertEmoji: (item: IEmojiType) => insertEmoji(item),
+        getValue: () => {
+          const editValue = editor.getText(editRef.current);
+          // 返回输入框信息
+          return transforms.editTransformSpaceText(editValue);
+        },
+        setValue: (val) => {
+          if (!val || !editRef.current) return;
+          // 把文本标签转义：如<div>[爱心]</div> 把这个文本转义为"&lt;div&lt;", newCurrentText 当前光标的节点元素的值
+          const repContent = labelRep(val);
+          editor.setText(editRef.current, repContent, () => {
+            const val = editor.getText(editRef.current);
+            // 控制提示
+            setTipHolder(val == "");
+            // 返回输入框信息
+            restProps.onChange?.(transforms.editTransformSpaceText(val));
+          });
+        },
+        clear: () => {
+          const curDom = clearEditor();
+          // 设置光标位置
+          setRangePosition(curDom, 0, true);
+          // 失去焦点
+          editRef?.current?.blur();
+          restProps.onChange?.("");
+        },
+        focus: () => {
+          requestAnimationFrame(() => {
+            editRef?.current?.focus();
+          });
+        },
+        blur: () => editRef?.current?.blur()
+      }) as IEditableRef
+  );
+
+  return (
+    <div className="fb-editor-container">
+      <div className="fb-editor-scroll">
+        <div
+          className="fb-editor-wrapper"
+          ref={editRef}
+          contentEditable
+          data-fish-editor
+          spellCheck
+          onPaste={onEditorPaste}
+          onBlur={onEditorBlur}
+          onFocus={onEditorFocus}
+          onInput={onEditorChange}
+          onCopy={onCopy}
+          onCut={onCut}
+          onKeyDown={onEditorKeydown}
+          onKeyUp={onEditorKeyUp}
+          onCompositionStart={onCompositionStart}
+          onMouseDown={onEditorMouseDown}
+          onCompositionEnd={onCompositionEnd}
+          onClick={(e) => {
+            e.preventDefault();
+            onEditorClick(e);
+            restProps.onClick?.();
+          }}
+          onDrop={(e) => {
+            // 禁用拖放操作, 如果拖动输入框内的图片，会导致吧图片的地址输入到 富文本中
+            e.preventDefault();
+            return false;
+          }}
+          onDragOver={(e) => {
+            // 禁用拖放操作， 如果拖动输入框内的图片，会导致吧图片的地址输入到 富文本中
+            e.preventDefault();
+            return false;
+          }}
+          onContextMenu={(e) => {
+            // 禁止鼠标右键
+            e.preventDefault();
+          }}
+        />
+      </div>
+      <div className="fb-tip-placeholder" style={{ display: showTipHolder ? "block" : "none" }}>
+        {placeholder == "" ? "" : placeholder || "请输入发送的消息"}
+      </div>
+    </div>
+  );
+});
+
+export default Editable;
